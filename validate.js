b928082c598b54a5fd7b923e66f9cc253cf778eb
Chris Maloney <maloneyc@ncbi.nlm.nih.gov>
2015-09-28 17:00:11 -0400

    Fixes #4. Implemented version checker, based on the processing instruction.

--- validate.js
+++ validate.js
@@ -40,6 +40,9 @@ var onSaxonLoad = function() {
   // Console for output; see class definition below
   var results = jats4r.results = new Results();
 
+  // This document's preference for the version
+  var jats4r_version;
+
   clear_input_file();
   clear_input_url();
 
@@ -71,8 +74,16 @@ var onSaxonLoad = function() {
 
 
 
-  // First fetch the DTDs database, then add event handlers
-  jats4r.jats_schema.read_database("jats-schema.yaml")
+  // First fetch the jats4r schema versions file, the jats schema database, 
+  // then add event handlers
+  fetch('schema/versions.yaml')
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(yaml_str) {
+      jats4r.jats4r_schema_versions = jsyaml.load(yaml_str);
+      return jats4r.jats_schema.read_database("jats-schema.yaml")
+    })  
     .then(function(db) {
       // FIXME: couldn't I set this inside the jats-schema module?
       jats4r.jats_schema_db = db;
@@ -452,8 +463,14 @@ var onSaxonLoad = function() {
     // doctype declarations, xml-model PIs, and XSD attributes on the root node)
     var schema_refs = jats4r.parser.parse_header(contents);
 
-    var jats4r_version = null,
-        count = 0;
+    // Figure out what JATS4R schema version to use
+
+    var count = 0,
+        schema_versions = jats4r.jats4r_schema_versions,
+        default_ver = schema_versions[schema_versions.length - 1];
+
+    jats4r_version = null;
+
     schema_refs.forEach(function(sr) {
       if (!sr.is_jats) {
         count++;
@@ -461,20 +478,52 @@ var onSaxonLoad = function() {
       }
     });
 
+    var message_given = false;  // keep track of whether or not we've put out a message
     if (!jats4r_version) {
+      message_given = true;
       results.warn(
         "JATS4R-compliant articles should use the xml-model processing instruction to " +
         "specify that they comply with our recommendations. For example: " + 
         "<blockquote><pre>&lt;?xml-model href=\"http://jats4r.org/schema/1.0/jats4r.sch\"\n" +
-        "  schematypens=\"http://purl.oclc.org/dsdl/schematron\" title=\"JATS4R 1.0\"?></pre></blockquote>"
+        "  schematypens=\"http://purl.oclc.org/dsdl/schematron\" title=\"JATS4R 1.0\"?></pre></blockquote>" +
+        "<p>This tool will now use the default version: '" + default_ver + "'."
       );
     }
     else if (count > 1) {
+      message_given = true;
       results.error(
         "Two or more &lt;?xml-model?> processing instructions referencing JATS4R " +
         "were found. Only the first will be used."
       );
     }
+
+    if (jats4r_version) {
+      var version_found = schema_versions.find(function(v) {
+        return v == jats4r_version;
+      });
+      if (!version_found) {
+        message_given = true;
+        results.error(
+          "A JATS4R &lt;?xml-model?> processing instruction was found, but the " +
+          "version number ('" + jats4r_version + "') does not appear to be valid.\n" +
+          "The valid version numbers are: " + schema_versions.join(", ") + ".\n" +
+          "This tool will now use the default version: '" + default_ver + "'."
+        );
+        jats4r_version = null;
+      }
+    }
+
+    if (!jats4r_version) jats4r_version = default_ver;
+
+    if (!message_given && count == 1) {
+      results.info(
+        "A JATS4R &lt;?xml-model?> processing instruction was found, specifying version '" +
+        jats4r_version + "'. This is good!"
+      );
+    }
+
+
+
 
     var jats_schema_ref = null,
         count = 0,
@@ -694,7 +743,7 @@ var onSaxonLoad = function() {
           //console.log("About to Saxon.run");
 
           var run_result = Saxon.run({
-            stylesheet: 'generated-xsl/0.1/' + xslt[level],
+            stylesheet: 'generated-xsl/' + jats4r_version + '/' + xslt[level],
             source: doc,
             method: 'transformToDocument',
             success: function(processor) {
